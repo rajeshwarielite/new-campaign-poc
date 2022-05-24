1ad70412dae20b09694358daf750e811f61b8973
rajeshwarielite <105626428+rajeshwarielite@users.noreply.github.com>
2022-05-24 18:46:04 +0530

    added new componet codes

--- src/app/components/campaign-container/campaign-container.component.ts
+++ src/app/components/campaign-container/campaign-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SaveCampaignModel } from 'src/app/services/new-campaign/models/new-campaign-models';
+import { NewCampaignService } from 'src/app/services/new-campaign/new-campaign.service';
 
 @Component({
   selector: 'app-campaign-container',
@@ -7,22 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampaignContainerComponent implements OnInit {
 
+  defineSelected: boolean = true;
   channelSelected: boolean = false;
-  constructor() { }
+  deploySelected: boolean = false;
+  resultSelected: boolean = false;
+
+  // @ts-ignore
+  saveCampaignModel: SaveCampaignModel = {};
+
+  constructor(private newCampaignService: NewCampaignService) { }
 
   ngOnInit(): void {
+    this.newCampaignService.$saveCampaignModel.subscribe(result => this.saveCampaignModel = result);
   }
 
-  defineTabSelected():void{
+  defineTabSelected(): void {
     this.channelSelected = false;
   }
 
-  channelTabSelected():void{
+  channelTabSelected(): void {
     this.channelSelected = true;
   }
 
   setChannelStep(next: boolean) {
     this.channelSelected = next;
+    if(next){
+     this.defineSelected = false;
+     this.deploySelected = false;
+     this.resultSelected = false;
+    }
+  }
+
+  setDeployStep(next: boolean) {
+    this.deploySelected = next;
+    if(next){
+     this.defineSelected = false;
+     this.channelSelected = false;
+     this.resultSelected = false;
+    }
+  }
+
+  setResultStep(next: boolean) {
+    this.resultSelected = next;
+    if(next){
+     this.defineSelected = false;
+     this.deploySelected = false;
+     this.channelSelected = false;
+    }
   }
 
 }
